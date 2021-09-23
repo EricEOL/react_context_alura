8db702f0f722e4584ffda72b3057430579433dd0
EricEOL <lopes.eric051@gmail.com>
2021-09-23 20:12:00 -0300

    Insert payment types in cart

--- src/pages/Carrinho/index.js
+++ src/pages/Carrinho/index.js
@@ -1,11 +1,11 @@
-import { Button, Snackbar, InputLabel } from '@material-ui/core';
+import { Button, Snackbar, InputLabel, Select, MenuItem } from '@material-ui/core';
 import MuiAlert from '@material-ui/lab/Alert';
 import { useCartContext } from 'common/context/Cart';
 import { PaymentContext } from 'common/context/Payment';
 import Produto from 'components/Produto';
 import { useContext, useState } from 'react';
 import { useHistory } from 'react-router';
-import { Container, Voltar, TotalContainer, PagamentoContainer} from './styles';
+import { Container, Voltar, TotalContainer, PagamentoContainer } from './styles';
 
 function Carrinho() {
   const history = useHistory();
@@ -14,7 +14,7 @@ function Carrinho() {
   const { paymentTypes, paymentType, setPaymentType } = useContext(PaymentContext)
   return (
     <Container>
-      <Voltar 
+      <Voltar
         onClick={() => history.goBack()}
       />
       <h2>
@@ -22,21 +22,31 @@ function Carrinho() {
       </h2>
       <PagamentoContainer>
         <InputLabel> Forma de Pagamento </InputLabel>
+        <Select 
+          value={paymentType.id}
+          onChange={(event) => setPaymentType(event.target.value)}
+        >
+          {paymentTypes.map(payment => (
+            <MenuItem value={payment.id} key={payment.id}>
+              {payment.name}
+            </MenuItem>
+          ))}
+        </Select>
       </PagamentoContainer>
       <TotalContainer>
-          <div>
-            <h2>Total no Carrinho: </h2>
-            <span>R$ </span>
-          </div>
-          <div>
-            <h2> Saldo: </h2>
-            <span> R$ </span>
-          </div>
-          <div>
-            <h2> Saldo Total: </h2>
-            <span> R$ </span>
-          </div>
-        </TotalContainer>
+        <div>
+          <h2>Total no Carrinho: </h2>
+          <span>R$ </span>
+        </div>
+        <div>
+          <h2> Saldo: </h2>
+          <span> R$ </span>
+        </div>
+        <div>
+          <h2> Saldo Total: </h2>
+          <span> R$ </span>
+        </div>
+      </TotalContainer>
       <Button
         onClick={() => {
           setOpenSnackbar(true);
@@ -44,28 +54,28 @@ function Carrinho() {
         color="primary"
         variant="contained"
       >
-         Comprar
-       </Button>
-        <Snackbar
-          anchorOrigin={
-            { 
-              vertical: 'top',
-              horizontal: 'right'
-            }
+        Comprar
+      </Button>
+      <Snackbar
+        anchorOrigin={
+          {
+            vertical: 'top',
+            horizontal: 'right'
           }
-          open={openSnackbar}
+        }
+        open={openSnackbar}
+        onClose={() => setOpenSnackbar(false)}
+      >
+        <MuiAlert
           onClose={() => setOpenSnackbar(false)}
+          severity="success"
         >
-           <MuiAlert
-            onClose={() => setOpenSnackbar(false)}
-            severity="success"
-          >
-            Compra feita com sucesso!
-          </MuiAlert>
-        </Snackbar>
+          Compra feita com sucesso!
+        </MuiAlert>
+      </Snackbar>
 
-        {cart.map(product => (
-        <Produto 
+      {cart.map(product => (
+        <Produto
           {...product}
           key={product.id}
         />
